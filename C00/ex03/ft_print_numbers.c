2a2b32168ac1be4678399e13f5d64c465b07a35c
Sergio <sergioseijas.ferreiro@gmail.com>
2026-04-21 11:35:39 +0200

    Ejercicio 03 C00

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sseijas <sergioseijas.ferreiro@gmail.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/21 11:16:16 by sseijas           #+#    #+#             */
+/*   Updated: 2026/04/21 11:34:57 by sseijas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char	n_i;
+
+	n_i = '0';
+	while (n_i <= '9')
+	{
+		ft_putchar(n_i);
+		n_i++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
