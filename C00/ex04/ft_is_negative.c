b0ce556c6e76cb5d6fe367d06d735020bf0bf0b2
Sergio <sergioseijas.ferreiro@gmail.com>
2026-04-21 11:49:28 +0200

    Subiendo ej 04 C00 con pruebas

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sseijas <sergioseijas.ferreiro@gmail.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/21 11:37:15 by sseijas           #+#    #+#             */
+/*   Updated: 2026/04/21 11:48:59 by sseijas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+	{
+		ft_putchar('P');
+	}
+	else
+	{
+		ft_putchar('N');
+	}
+}
+
+int	main(void)
+{
+	ft_is_negative(42);
+	ft_is_negative(0);
+	ft_is_negative(-5);
+	return (0);
+}
