6951ea7bda79853df0e7cf4f1114f339491206df
Sergio <sergioseijas.ferreiro@gmail.com>
2026-04-21 17:12:12 +0200

    Subiendo ej05 C00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sseijas <sergioseijas.ferreiro@gmail.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/21 16:05:45 by sseijas           #+#    #+#             */
+/*   Updated: 2026/04/21 17:11:08 by sseijas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_3_putchar(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_3_putchar(a, b, c);
+				if (!(a == '7' && b == '8' && c == '9'))
+				{
+					write(1, ", ", 2);
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
