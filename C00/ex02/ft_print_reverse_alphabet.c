7344ed2f81961f10e4fcc4dad977697b266d067e
Sergio <sergioseijas.ferreiro@gmail.com>
2026-04-21 11:03:38 +0200

    Subiendo ej 02 C00

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char letra;
+	letra = 'z';
+	while(letra >= 'a')
+	{
+		ft_putchar(letra);
+		letra--;
+	}
+}
+
+/*int main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
